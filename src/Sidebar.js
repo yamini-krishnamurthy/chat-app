96caa3559ad55ca83835408e56031360f73c3650
Yamini Krishnamurthy <ykrishna@purdue.edu>
2018-07-03 09:50:26 -0400

    Add Chat and Sidebar components

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="sidebar">
+        Sidebar
+      </div>
+    )
+  }
+}
+
+export default Sidebar
