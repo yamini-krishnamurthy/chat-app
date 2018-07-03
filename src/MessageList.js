caa181236f0a93994b148bf43422733e828e88a3
Yamini Krishnamurthy <ykrishna@purdue.edu>
2018-07-03 10:45:54 -0400

    Add a MessageList component that contains Message components

---
+++ src/MessageList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Message from './Message'
+
+const MessageList = () => {
+  const messages = [
+    {id: 1, userName: 'yamini', body: '\'sup'},
+    {id: 2, userName: 'charlene', body: 'nothin\' much'}
+  ]
+  return (
+    <div className="MessageList">
+      {messages.map(msg => (
+        <Message key={msg.id} message={msg} /> 
+      ))}
+    </div>
+  )
+}
+
+export default MessageList
