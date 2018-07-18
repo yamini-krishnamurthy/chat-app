55fbaf5b0fe1a5dc5783f776fc0bb4f7e922d5eb
Yamini Krishnamurthy <ykrishna@purdue.edu>
2018-07-18 18:00:10 +0530

    Add an emoji reaction button

--- src/Message.js
+++ src/Message.js
@@ -1,33 +1,59 @@
 import React from 'react'
 
+import { css, StyleSheet } from 'aphrodite'
 import Avatar from './Avatar'
 import Metadata from './Metadata'
 
 const Message = (props) => {
   return (
-    <div className="Message" style={styles.message}>
+    <div className={`Message ${css(styles.message)}`}>
       <Avatar user={props.message.user} />
-      <div className="details" style={styles.details}>
+      <div className={css(styles.details)}>
         <Metadata message={props.message} />
         <div className="body">
           {props.message.body}
         </div>
+        <button className={`reactionButton ${css(styles.reactionButton)}`}>
+          <i className="far fa-smile"></i>
+        </button>
       </div>
     </div>
   )
 }
 
-const styles = {
+const styles = StyleSheet.create({
   message: {
     display: 'flex',
     marginTop: '1rem',
-    padding: '0 1rem',
+    padding: '1rem 1rem',
+    position: 'relative',
+
+    ':hover': {
+      backgroundColor: '#f6f6f6',
+    },
   },
 
   details: {
     flex: 1,
     paddingLeft: '0.5rem',
   },
-}
+
+  reactionButton: {
+    position: 'absolute',
+    border: 0,
+    outline: 0,
+    padding: 0,
+    backgroundColor: 'transparent',
+    fontSize: '1rem',
+    top: '0.5rem',
+    right: '0.5rem',
+    cursor: 'pointer',
+    color: '#ccc',
+
+    ':hover': {
+       color: '#3366ff',
+    },
+  },
+})
 
 export default Message
